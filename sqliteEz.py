b959feb6dde594e5d548e1a31bfc14f3bd7b2c34
d_cuzya <d.cuzya07@gmail.com>
2024-09-11 16:20:29 +0300

    a

--- sqliteEz.py
+++ sqliteEz.py
@@ -26,20 +26,18 @@ class sqliteEz:
         #Создание таблицы "CurrentOrders"
         self.cur.execute("CREATE TABLE IF NOT EXISTS CurrentOrders ("
                          "ID INTEGER PRIMARY KEY AUTOINCREMENT, "
-                         "OrderID INTEGER, "
+                         "FoodID INTEGER, "
                          "UserID INTEGER, "
                          "PaidFor INTEGER NOT NULL DEFAULT 0, " #Оплаченный ли заказ, 1 - true, 0 - false, Default: 0
                          "Completed INTEGER NOT NULL DEFAULT 0, " #Выполнен ли заказ, 1 - true, 0 - false, Default: 0
-                         "FOREIGN KEY (OrderID) REFERENCES Foods (ID), "
+                         "FOREIGN KEY (FoodID) REFERENCES Foods (ID), "
                          "FOREIGN KEY (UserID) REFERENCES UserList (ID) "
                          ");")
         #Создание таблицы "History"
         self.cur.execute("CREATE TABLE IF NOT EXISTS History ("
                          "ID INTEGER PRIMARY KEY AUTOINCREMENT, "
                          "OrderID INTEGER, "
-                         "CurrentUserID INTEGER, "
-                         "FOREIGN KEY (OrderID) REFERENCES Foods (ID), "
-                         "FOREIGN KEY (CurrentUserID) REFERENCES CurrentOrders (ID) "
+                         "FOREIGN KEY (OrderID) REFERENCES CurrentOrders (ID) "
                          ");")
         self.con.commit()
         
@@ -51,10 +49,20 @@ class sqliteEz:
     #Получение значение колонки из таблицы с определённым условием 
     #_Table - название требуемой таблицы, _ColumnForCondition - Колонка к которому будет преминятся условие, _ValueForCondition - какое значение должно быть у колонки, _ReturnColumn - какую колонку вернуть
     def getColumnValueByCondition(self, _Table, _ColumnForCondition, _ValueForCondition, _ReturnColumn):
-        self.cur.execute(f"SELECT \"{_ReturnColumn}\" FROM \"{_Table}\" WHERE \"{_ColumnForCondition}\" = \"{_ValueForCondition}\";", ())
-        return self.cur.fetchall()[0][0]
+        try:
+            return self.cur.execute(f"SELECT \"{_ReturnColumn}\" FROM \"{_Table}\" WHERE \"{_ColumnForCondition}\" = \"{_ValueForCondition}\";", ()).fetchall()[0][0]
+        except:
+            return None
     
     #Функция добовления строки в таблицу Foods
     def addFoods(self, _Name, _Description = "", _Price = "", _Img = ""):
         self.cur.execute("INSERT INTO Foods (Name, Description, Price, Img) VALUES (?, ?, ?, ?);", (_Name, _Description, _Price, _Img))
+        self.con.commit()
+    #Функция добовления юзера
+    def addUser(self, _FirstName, _SecondName):
+        self.cur.execute("INSERT INTO UserList (FirstName, SecondName) VALUES (?, ?);", (_FirstName, _SecondName))
+        self.con.commit()
+    #Функция добовления заказа
+    def addOrder(self, ):
+        self.cur.execute()
         self.con.commit()
